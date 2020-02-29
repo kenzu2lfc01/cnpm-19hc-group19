21f70cc1d92dc35e91a423d76a1dfd6b7723336c
thaivo512 <thaivo512@gmail.com>
2020-02-29 16:22:19 +0700

    about gallery menu

--- web-app/js/guest.js
+++ web-app/js/guest.js
@@ -1,12 +1,74 @@
 let toggle = $('.toggle-menu');
-let menu = $('.menu-list');  
-$('.menu-item').click(function() {
-    $('.menu-item.active').removeClass('active');
-    $(`.menu-${$(this).attr('href').substring(1)}`).addClass('active'); 
-    if(!['none', '0px'].includes(menu.css('max-height'))) toggle.click();  
-})
+let menu = $('.menu-list');
+let navHeader = $('.nav-header');
+let navMenu = $('.nav-menu');  
+let offsetTop = navMenu.offset().top + navMenu.outerHeight();
+let mainNavLinks = $('.menu-item'); 
+
 toggle.click((e) => { 
     toggle.toggleClass('active');
     if(menu.css('max-height') != '0px') menu.css('max-height', '');
     else menu.css('max-height', menu.prop('scrollHeight'));
+})
+$(window).scroll(function() {
+    let current = window.scrollY + navMenu.outerHeight()*2; 
+    mainNavLinks.each(function() {  
+        let section = $($(this).prop('hash')); 
+        let top = section.offset().top <= current;
+        let bottom = section.offset().top + section.outerHeight() > current;
+        if (top && bottom) $(this).addClass("active");
+        else $(this).removeClass("active"); 
+    });    
+
+    if(window.pageYOffset > offsetTop) {
+        navHeader.addClass('hide');
+        navMenu.addClass('sticky');
+    }
+    else{
+        navHeader.removeClass('hide');
+        navMenu.removeClass('sticky');
+    }    
+
+    if(toggle.hasClass('active')) toggle.click();
+})
+
+
+$('.gallery .owl-carousel').owlCarousel({
+    items: 2,
+    loop: true,
+    margin: 5,
+    dots: false,
+    nav: true,
+    navText: ['<i class="fa fa-angle-left"></i>', '<i class="fa fa-angle-right"></i>'],
+    autoplay: true,
+    responsiveClass: true,
+    responsive: {
+        0: {
+            items: 1,
+            nav: false
+        }, 
+        950: {
+            items: 2,
+            nav: true
+        }
+    }
+});
+
+$('.owl-carousel.list-food').owlCarousel({  
+    loop: true, 
+    center: true,
+    dots: false,
+    autoWidth: true,
+    callbacks: true,
+    margin: -130,
+    autoplay: false,
+    autoplayTimeout: 1000,
+    autoplayHoverPause: true
+}); 
+
+
+
+$('.tab-food-item').click(function(){
+    $('.tab-food-item').removeClass('active');
+    $(this).addClass('active');
 })
