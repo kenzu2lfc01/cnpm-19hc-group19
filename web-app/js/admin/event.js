56d1f289aabd0ae37865ecd38191fc132f10e3f9
thaivo512 <thaivo512@gmail.com>
2020-03-03 23:28:24 +0700

    login logout forgot password

--- web-app/js/admin/event.js
+++ web-app/js/admin/event.js
@@ -1,11 +1,63 @@
-$('.btn-toggle i').click(function(){
-    toggleSidebar();
-})
-$('.menu-parent').click(function() { 
-    let current = $(this).hasClass('active');
-    $('.menu-parent.active').removeClass('active'); 
-    if(!current)  $(this).toggleClass('active');
-})
+window.addEventListener('resize', adjustSidebar);
+window.addEventListener('load', loadRequired); 
+window.addEventListener('click', triggerWindowClick);
+$('.btn-toggle i').click(toggleSidebar)
+$('.menu-parent').click(function() { toggleMenu($(this)); })
+$('.btn-forgot').click(function(){ $('.login').toggleClass('forgot'); })
+$('.logout').click(logout)
+ 
+let processLogin = false;
+$('#log-in').submit(function(e) { login(e, $(this).serialize()); });
+let processResetPass = false;
+$('#forgot-password').submit(function(e) { resetPassword(e, $(this).serialize()); });
+
+
+function resetPassword(e, d){
+    e.preventDefault();
+    if(processResetPass) return;
+    processResetPass = true;
+ 
+    $.ajax({
+        type: "POST",
+        url: 'http://5e5a5ce16a71ea0014e61d69.mockapi.io/infoWeb',
+        data: d,
+        success: function(data)
+        { 
+            pushNotify('Check your mail to get password');
+            processResetPass = false;
+        }
+    });
+}
+function login(e, d){
+    e.preventDefault();
+    if(processLogin) return;
+    processLogin = true;
+ 
+    $.ajax({
+        type: "POST",
+        url: 'http://5e5a5ce16a71ea0014e61d69.mockapi.io/infoWeb',
+        data:  d,
+        success: function(data)
+        {  
+            if(data.username) {
+                localStorage.token = "login success";
+                toggleLogin();
+            }
+            else {
+                pushNotify(data.name);
+            }
+            processLogin = false;
+        }
+    });
+}
+function toggleLogin(){
+    $('.login').toggleClass('login-required');
+}
+function logout(){
+    event.preventDefault();
+    localStorage.removeItem('token');
+    window.location = './index.html';
+}
 function toggleSidebar(){
     if(window.innerWidth < 1000){
         $('.sidebar').addClass('collapse');
@@ -16,6 +68,11 @@ function toggleSidebar(){
         $('.sidebar').toggleClass('collapse');
     }
 }
+function toggleMenu(e){
+    let current = e.hasClass('active');
+    $('.menu-parent.active').removeClass('active'); 
+    if(!current)  e.toggleClass('active');
+}
 function adjustSidebar(){
     if (window.innerWidth > 1000) { 
         $('.sidebar').removeClass('collapse');
@@ -25,19 +82,28 @@ function adjustSidebar(){
         $('.sidebar').addClass('hidden'); 
     } 
 }
+function loadRequired(){
+    if(!localStorage.token) toggleLogin();
 
-window.addEventListener('resize', adjustSidebar);
-window.addEventListener('load', function(){
     let page = $('.main').attr('page');
     let current = $( `a[href*='${page}']` );
     current.addClass('current-page');
     current.parent().parent().addClass('current-module');
+
     adjustSidebar();
-}); 
-window.addEventListener('click', function(event){
+}
+function triggerWindowClick(event){
     let parent = $('.profile-button');
     if(parent.get(0) == event.target || parent.has(event.target).length)
         $('.profile-dropdown').toggleClass('hide');
     else 
         $('.profile-dropdown').addClass('hide');
-});
+}
+function pushNotify(notify){
+    let id = Math.random().toString(36).replace('.', '');
+    $('#notification').append(` <div id=${id}>${notify}</div> `); 
+    $("#notification").animate({
+        scrollTop: $("#notification")[0].scrollHeight
+    }, 100);  
+    setTimeout(() => $(`#${id}`).remove(), 6100);
+}
